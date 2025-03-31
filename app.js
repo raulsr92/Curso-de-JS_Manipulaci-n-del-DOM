baaed981d35367a9f3948be22682d408e0d8ac77
Raul Sanchez R. <raul.sr92@gmail.com>
2025-03-31 18:20:47 -0500

    Clase 4: uso de getElementById y querySelector

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
 let myName = "Raul Adolfo"
 
 console.log(myName)
+
+// Clase 4: método getElementById( )
+
+let tituloPrincipal = document.getElementById("app-title");
+
+let titulo2 = document.querySelector("#app-title");
+
+titulo2.innerHTML= "Bienvenidos a mi App"+" "+myName
