b678d7518259b59054ba7fa60b967ab98f9b6fea
Raul Sanchez R. <raul.sr92@gmail.com>
2025-04-04 16:24:35 -0500

    Clase 5 Navegación efectiva entre nodos: propiedades parenElement y parentNode y el método closest( )

--- appClase5.js
+++ appClase5.js
@@ -32,3 +32,33 @@ console.log(ultimoHijo)
 
 const tercerHijo = ultimoHijo.previousElementSibling
 console.log(tercerHijo)
+
+
+const previo = elementULPadre.previousElementSibling
+console.log(previo)
+
+
+// Tarea N° 02: Seleccionar elemento Hijo para desde ahí acceder a padres
+
+let firstElementLI = document.querySelector("li")
+
+console.log(firstElementLI)
+
+// Acceder a su padre
+
+//>> Forma 1
+let parentOfLi = firstElementLI.parentElement
+
+console.log(parentOfLi);
+
+//>> Forma 2
+
+let parentOfLi2 = firstElementLI.parentNode
+
+console.log(parentOfLi2);
+
+//>> Forma 3
+
+let parentOfLi3 = firstElementLI.closest("#parent")
+
+console.log(parentOfLi3);
