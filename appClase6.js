ba99cbb9c9ef66c265091d4c58539108c1a4554a
Raul Sanchez R. <raul.sr92@gmail.com>
2025-04-07 16:33:22 -0500

    Clase 7: Modificando texto en HTML

--- appClase6.js
+++ appClase6.js
@@ -18,3 +18,17 @@ console.log(valorInput)
 valorInput = inputElement.value = "Raul Sanchez"
 
 console.log(valorInput)
+
+// Clase 7: 
+
+let tituloProject = document.getElementById("app-title");
+
+console.log(tituloProject)
+console.dir(tituloProject)
+
+
+//--Cambiar título de forma dinámica
+
+tituloProject.textContent = "Soy el nuevo título m+as dinámico"
+
+tituloProject.innerText =  "Otra vez cambié el título. Sigo dinámico"
